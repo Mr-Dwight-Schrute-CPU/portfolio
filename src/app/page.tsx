07d21836371770c267ba28da1dcaeaba4a872fbb
KunalNasa <kunalnasa.dev@gmail.com>
2025-01-29 19:07:55 +0530

    added site metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,25 @@ import AboutMe from "@/components/AboutMe";
 import MySkills from "@/components/MySkills";
 import MyProjects from "@/components/MyProjects";
 
+export const metadata = {
+  title: 'Kunal Nasa - Portfolio',
+  description: "Hey, I'm Kunal Nasa! Explore my portfolio, skills, and projects.",
+  keywords: ['Kunal Nasa', 'Portfolio', 'Web Developer', 'AI', 'Next.js', 'MERN', 'React.js', 'MongoDB', 'Express', 'Node.js', 'Postgres', 'SQL'],
+  authors: [{ name: 'Kunal Nasa' }],
+  openGraph: {
+    title: 'Kunal Nasa - Portfolio',
+    description: 'Check out my portfolio, projects, and skills.',
+    url: 'https://kunalnasa.xyz',
+    images: [
+      {
+        url: '/Images/AppLogo.jpeg',
+        width: 800,
+        height: 600,
+        alt: 'Kunal Nasa Portfolio Preview',
+      },
+    ],
+  },
+};
 export default function Home() {
   return (
     <div className="relative h-auto w-full p-5 flex flex-col mx-auto md:w-8/12">
